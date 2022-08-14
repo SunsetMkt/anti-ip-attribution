a7dbd82e5d6f2179798b81565e3276b6da7a1a85
Sunset <26019675+lwd-temp@users.noreply.github.com>
2022-08-14 22:37:19 +0800

    生成脚本自动检查

--- generate.py
+++ generate.py
@@ -12,6 +12,7 @@
 # surge.list Surge分流规则
 # quantumultx.list QuantumultX分流规则
 import os
+import sys
 
 import yaml
 
@@ -53,6 +54,67 @@ def get_head_comment(config, filename='', description=''):
 def seprate_comma(string):
     '''分割字符串'''
     return string.split(',')
+
+
+def check_rules(config):
+    '''检查规则是否有误'''
+    rules = config['config']['rules']
+    # 预期中可用的规则方法
+    expected_methods = ['DOMAIN', 'DOMAIN-SUFFIX',
+                        'DOMAIN-KEYWORD', 'IP-CIDR', 'GEOIP', 'DST-PORT']
+    # 容易出错的规则方法
+    questioned_methods = ['SRC-IP-CIDR', 'SRC-PORT', 'RULE-SET', 'MATCH']
+    # 预期中可用的规则部分
+    expected_rules = ['DIRECT', 'REJECT', 'no-resolve']
+    # rules中不需要出现IP类关键词，生成脚本会按需自动添加
+    should_not_appear_rules = ['IP', 'IP归属地']
+    for rule in rules:
+        rule = rule.strip()
+        print(rule)
+        # 检查空行
+        if rule == '':
+            print('规则空行')
+            return False
+        else:
+            print('非空行')
+        # 检查逗号分隔
+        length = len(seprate_comma(rule))
+        print('length: ' + str(length))
+        if length == 1:
+            print('规则不完整？没有逗号分隔？：' + rule)
+            return False
+        elif length == 2:
+            pass
+        elif length == 3:
+            pass
+        else:
+            print('规则有误？逗号分隔超过三个部分：' + rule)
+            return False
+        # 检查规则方法
+        method = seprate_comma(rule)[0]
+        print('method: ' + method)
+        if method not in expected_methods and method not in questioned_methods:
+            print('规则方法有误？：' + rule)
+            return False
+        if method.upper() in questioned_methods:
+            print('规则方法有误？' + method + '可能是常见错误：' + rule)
+            return False
+        if method not in expected_methods and method.upper() in expected_methods:
+            print('规则方法未大写？：' + rule)
+            return False
+        # 检查规则部分
+        if length == 3:
+            print('length == 3')
+            this_rule = seprate_comma(rule)[2]
+            print('rule: ' + this_rule)
+            if this_rule not in expected_rules:
+                print('规则有误？' + this_rule + '是预期之外的规则：' + rule)
+                return False
+            for should_not_appear_rule in should_not_appear_rules:
+                if should_not_appear_rule in this_rule.upper():
+                    print('规则有误？rules中不需要出现“IP归属地”类规则描述：' + rule)
+                    return False
+    return True
 
 
 def generate_parser(config):
@@ -114,13 +176,29 @@ def generate_quantumultx(config):
 if __name__ == '__main__':
     print('开始生成配置文件...')
     config = read_yaml('rules.yaml')
+    print('=====================')
     print(config)
-    print('生成parser.yaml...')
-    generate_parser(config)
-    print('生成rule-provider.yaml...')
-    generate_rule_provider(config)
-    print('生成surge.list...')
-    generate_surge(config)
-    print('生成quantumultx.list...')
-    generate_quantumultx(config)
-    print('生成配置文件完成！')
+    print('=====================')
+    print('检查规则是否有误...')
+    if check_rules(config):
+        print('规则检查通过！')
+        print('=====================')
+        print('生成parser.yaml...')
+        generate_parser(config)
+        print('=====================')
+        print('生成rule-provider.yaml...')
+        generate_rule_provider(config)
+        print('=====================')
+        print('生成surge.list...')
+        generate_surge(config)
+        print('=====================')
+        print('生成quantumultx.list...')
+        generate_quantumultx(config)
+        print('=====================')
+        print('生成配置文件完成！')
+    else:
+        print('规则检查未通过！')
+        print('=====================')
+        print('请检查规则是否有误！')
+        print('生成配置文件失败！')
+        sys.exit(1)
